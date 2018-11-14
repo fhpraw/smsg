ffa61cbfcae65f6e90b203bab3e72f292b78f44a
Fajar Hari Prabowo <code.fhp@gmail.com>
2018-11-14 21:01:50 +0700

    replace xlsx support with csv support

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-# Automate boring stuff like sending massive SMS (on Windows)
-
-This is a simple program for sending mass SMS.
+# Automate boring stuff on sending SMS (Windows)
 
 ## Preparing Gammu
 1. Install [Python 3.6](https://www.python.org/downloads/windows/) 64 bit for Windows
@@ -10,23 +8,33 @@ This is a simple program for sending mass SMS.
 5. Plug the modem (I use Wavecom Fastrack M1306B)
 6. Create file named gammurc on current directory
 
+## How to create gammurc
+Create plain text file (no extension) in the same directory of this python script. Write this configuration script on gammurc file:
+```text
+[gammu]
+device=com4
+connection=at115200
+```
+`device=com4` is device location, it can be com5 or com3, that depends on which port you plug your modem on the PC.
+`connection=at115200`, just keep it as it is (tested for Wavecom Fastrack M1306B)
+
 ## How to use
 Massive SMS
 
 ```sh
-usage: smsbulk.py [-h] TEXT_CONTENT XLSX_PATH SHEET_NAME COLUMN_LOCATION
+usage: smsbulk.py [-h] TEXT_CONTENT CSV_PATH
+
+Send SMS to bulk cell number using list of cell numbers store in CSV file
 
 positional arguments:
-  TEXT_CONTENT     text content
-  XLSX_PATH        xlsx file which contain list of cellnumber
-  SHEET_NAME       active sheet name in xlsx file
-  COLUMN_LOCATION  column location in active sheet
+  TEXT_CONTENT  text content
+  CSV_PATH      csv file which contain list of cellnumber, no column header
+                please !!!
 
 optional arguments:
-  -h, --help       show this help message and exit
+  -h, --help    show this help message and exit
 ```
 
 ## TODO
-- add CSV support
 - single SMS support
 - set gammurc file
--- smsbulk.py
+++ smsbulk.py
@@ -1,40 +1,12 @@
 """SMS Bulk
 
-This program will help you to send mass SMS to a bunch of cell number store in XLSX file
+This program will help you to send mass SMS to a bunch of cell number store in CSV file
 """
 
 import sys
 import argparse
+import csv
 import gammu
-import openpyxl
-
-def get_column_values(file, sheet, column):
-    """Get all cell values from define column
-
-    Parameters:
-        file (str): The file location of the spreadsheet XLSX
-        sheet (str): Active sheet name
-        column (str): Column address
-    
-    Returns:
-        list: a list of cell value based on defined column address
-    """
-    list = []
-    try:
-        # load excel file
-        wb = openpyxl.load_workbook(file)
-    except FileNotFoundError:
-        # file not found
-        sys.exit('File not found')
-
-    # choose active sheet
-    sheet = wb[sheet]
-
-    # get cell value
-    for cell in sheet[column]:
-        list.append(cell.value)
-
-    return list
 
 def define_message(text, cellnum):
     """Create SMS message format in gammu
@@ -57,7 +29,23 @@ def define_message(text, cellnum):
         }
         return message
 
-def send_bulk_message(text, list_of_cellnum):
+def get_cellnumbers(file):
+    """Get cell numbers from CSV file
+    
+    Parameters:
+        file: csv file
+    
+    Returns:
+        list: list of cell numbers
+    """
+    list = []
+    with open(file) as csv_file:
+        reader = csv.reader(csv_file, delimiter=';')
+        for row in reader:
+            list.append(row[0])
+    return list
+            
+def send(text, list_of_cellnum):
     """Send mass SMS to multiple cell number
 
     Parameters:
@@ -92,12 +80,10 @@ def send_bulk_message(text, list_of_cellnum):
         print(f'The message has been succesfully sent to {cellnumber}')
         total_cellnumber -= 1
 
-parser = argparse.ArgumentParser(description='Send SMS to bulk cell number')
+parser = argparse.ArgumentParser(description='Send SMS to bulk cell number using list of cell numbers store in CSV file')
 parser.add_argument("TEXT_CONTENT", help="text content")
-parser.add_argument("XLSX_PATH", help="xlsx file which contain list of cellnumber")
-parser.add_argument("SHEET_NAME", help="active sheet name in xlsx file")
-parser.add_argument("COLUMN_LOCATION", help="column location in active sheet WARNING !!! WITHOUT HEADER")
+parser.add_argument("CSV_PATH", help="csv file which contain list of cellnumber, no column header please !!!")
 args = parser.parse_args()
 
-list_cellnum = get_column_values(args.XLSX_PATH, args.SHEET_NAME, args.COLUMN_LOCATION)
-send_bulk_message(args.TEXT_CONTENT, list_cellnum)
+list_cellnum = get_cellnumbers(args.CSV_PATH)
+send(args.TEXT_CONTENT, list_cellnum)
